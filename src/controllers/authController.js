249d1b2cc3f338da70adb1733b41bb49798453a1
AllanCFE <allan.echeverria@gmail.com>
2022-04-11 01:27:45 -0300

    Aula 2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,16 @@
+const bcryptjs = require("bcryptjs");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const User = require('../models/user')
 
 const router = express.Router()
+
+function generateToken (params = {}){
+    return jwt.sign( params, process.env.SECRET, {
+        expiresIn: 86400,
+    })
+}
 
 router.post('/register', async (req,res) => {
     let { email } = req.body
@@ -16,10 +24,23 @@ router.post('/register', async (req,res) => {
 
         user.password = undefined
 
-        return res.send( {user})
+        return res.send( {user, token: generateToken({id: user.id})})
     } catch {
         return res.status(400).send({ error: 'Registration failed' })
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password} = req.body
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) return res.status(400).send({ error: 'User not found!'})
+
+    if(!await bcryptjs.compare(password, user.password)) return res.status(400).send({ error: 'Invalid password!'})
+
+    user.password = undefined
+
+    res.send({user, token: generateToken({id: user.id})})
 })
 
 module.exports = app => app.use('/auth', router)
